73c6d8d89e2996df725eb45cbbf3622656fec3e8
tanat_malik <tanat.malik.93@mail.ru>
2025-05-10 23:22:11 +0500

    Обновление

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 user_choice = ''
 while True:
-    user_choice = input('Ты кто такой? ')
+    user_choice = input('Ты кто такой? Как зовут: ')
     if user_choice == '' or user_choice == ' ':
         print('Напиши че нить')
     else:
