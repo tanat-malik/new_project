d2d3af968bdf9bbf2590ef2f948b4b69a7b0844c
tanat_malik <tanat.malik.93@mail.ru>
2025-05-10 22:35:37 +0500

    Обновление

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print('Hello, World!')
+user_choice = ''
+while True:
+    user_choice = input('Ты кто такой? ')
+    if user_choice == '' or user_choice == ' ':
+        print('Напиши че нить')
+    else:
+        print(user_choice)
+        break
