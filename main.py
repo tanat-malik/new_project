b4fac806ea5fa465a275dbebbae58d87bef3c949
tanat_malik <tanat.malik.93@mail.ru>
2025-05-12 10:40:17 +0500

    update file

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-user_choice = ''
-while True:
-    user_choice = input('Ты кто такой? Как зовут: ')
-    if user_choice == '' or user_choice == ' ':
-        print('Напиши че нить')
-    else:
-        print(user_choice)
-        break
+# user_choice = ''
+# while True:
+#     user_choice = input('Ты кто такой? Как зовут: ')
+#     if user_choice == '' or user_choice == ' ':
+#         print('Напиши че нить')
+#     else:
+#         print(user_choice)
+#         break
+
+
+print('Hello, World!')
